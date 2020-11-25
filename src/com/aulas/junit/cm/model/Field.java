5f12f59e6e4ff9670d69dfd120d570e115839643
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-11-25 11:56:00 -0300

    Atributos da Classe Field

--- src/com/aulas/junit/cm/model/Field.java
+++ src/com/aulas/junit/cm/model/Field.java
@@ -1,5 +1,27 @@
 package com.aulas.junit.cm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
 
+	private final int x;
+	private final int y;
+	
+	private boolean opened = false;
+	private boolean mined = false;
+	private boolean marked = false;
+
+	// Vizinhos de campos com minas proximas, que podem ser eliminados 
+	private List<Field> neighbors = new ArrayList<Field>();
+	
+	
+	public Field(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	
+	
 }
